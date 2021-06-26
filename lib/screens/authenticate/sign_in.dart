f63b160a675d410815ba4b30114ea7c9b105f460
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-06-26 19:38:03 +0200

    sign in with registered email and password set up

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,10 +2,8 @@ import 'package:firebase_coffee_app/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-
   final Function toggleView;
   SignIn({required this.toggleView});
-
 
   @override
   _SignInState createState() => _SignInState();
@@ -13,10 +11,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = "";
   String password = "";
+  String error = "";
 
   @override
   Widget build(BuildContext context) {
@@ -43,41 +43,64 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              onChanged: (val) {
-                setState(() {
-                  email = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              obscureText: true,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(primary: Colors.pink[400]),
-              child: Text(
-                'Sign in',
-                style: TextStyle(color: Colors.white),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
               ),
-              onPressed: () async {},
-            )
-          ],
+              TextFormField(
+                validator: (val) => val!.isEmpty ? "Enter an email" : null,
+                onChanged: (val) {
+                  setState(() {
+                    email = val;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val!.length < 6 ? "Enter a password 6+ chars long" : null,
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() {
+                    password = val;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.pink[400]),
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    dynamic result =
+                        await _auth.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                        error = "Invalid credentials";
+                      });
+                    }
+                  }
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,6 +31,17 @@ class AuthService {
   }
 
   // sing in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _fromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // register with email and password
   Future registerWithEmailAndPassword(String email, String password) async {
