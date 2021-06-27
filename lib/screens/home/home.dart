8a8e9fb227d9d3242db915a4d77f57f6cc1d8614
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-06-27 22:16:36 +0200

    added the settings icon and the bottom sheet for updating the user preferences

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_coffee_app/models/brew.dart';
 import 'package:firebase_coffee_app/services/auth.dart';
 import 'package:firebase_coffee_app/services/database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text("Bottom Sheet"),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>?>.value(
       value: DatabaseService().brews,
       initialData: null,
@@ -23,11 +35,29 @@ class Home extends StatelessWidget {
           elevation: 0.0,
           actions: <Widget>[
             TextButton.icon(
-                icon: Icon(Icons.person),
-                label: Text('logout'),
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                label: Text(
+                  'logout',
+                  style: TextStyle(color: Colors.black),
+                ),
                 onPressed: () async {
                   await _auth.signOut();
-                })
+                }),
+            TextButton.icon(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                ),
+                label: Text(
+                  "settings",
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  _showSettingsPanel();
+                }),
           ],
         ),
         body: BrewList(),
