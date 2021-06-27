97011e4bc7b0ee797c7f96172af1b3349818a210
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-06-27 21:43:16 +0200

    added the brew tile widget to display the data from the db

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -3,6 +3,8 @@ import 'package:firebase_coffee_app/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
 
   @override
@@ -13,12 +15,10 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>?>(context);
-    brews!.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
 
-    return Container();
+    return ListView.builder(itemCount: brews!.length,
+      itemBuilder: (context, index){
+        return BrewTile(brew : brews[index]);
+      },);
   }
 }
